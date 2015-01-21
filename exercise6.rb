d5ee9c466edcc3e76edb5be0211813e5f52be888
Dominic Brugnano <dbgood@gmail.com>
2015-01-21 14:49:22 -0500

    Added rice to the original grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,10 @@
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+# Grocery list with items on each line with an * in front of each
+grocery_list.each {|item| puts "* #{item}"}
+
+# Add rice to the list
+grocery_list.push("rice")
 grocery_list.each {|item| puts "* #{item}"}
 
