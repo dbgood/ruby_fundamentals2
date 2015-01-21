a9cf19e30f11dc4d0248f242934fa4c4946b088a
Dominic Brugnano <dbgood@gmail.com>
2015-01-21 16:17:11 -0500

    Sorted list in alphabetical order

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,19 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-
 # Grocery list with items on each line with an * in front of each
 grocery_list.each {|item| puts "* #{item}"}
 
+def display_list(list)
+	list.each {|item| puts "* #{item}"}
+end
+
+
 # Add rice to the list
 grocery_list.push("rice")
-grocery_list.each {|item| puts "* #{item}"}
+display_list(grocery_list)
 
-puts grocery_list.count
+
+puts "the number of items are: #{grocery_list.count}"
 
 # Out put bananas "You need to pick up bananas"
 if grocery_list.include?("bananas")
@@ -18,3 +23,7 @@ puts "You need to pick up bananas"
 	end
 # Display the second item on the grocery list
 puts grocery_list[1]
+
+# Sort out grocery list alphabetically
+# puts grocery_list.sort
+display_list(grocery_list.sort)
